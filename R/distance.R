123b29465cc51385859b5f11cb5c29500bc83973
Santiago Marin <55192971+marinsantiago@users.noreply.github.com>
2025-02-27 12:01:01 +1100

    Delete R directory

--- R/distance.R
+++
@@ -1,7 +0,0 @@
-distance.tv <- function(target, approx, step = 0.001){
-  tv_distance(target, approx, step)
-}
-
-distance.ks <- function(target, approx, step = 0.001){
-  ks_distance(target, approx, step)
-}
--- R/emOptim.R
+++
@@ -1,9 +0,0 @@
-em.optim <- function(y, means, covs, probs, a, b, c, r,
-                    lik_weights, mean_weights, cov_weights, prob_weights,
-                    alphas, betas, lambdas, nus, psis,
-                    max_iters = 500, epss = 1e-4){
-
-  em_optim(y, means, covs, probs, a, b, c, r, lik_weights, mean_weights,
-           cov_weights, prob_weights,  alphas, betas, lambdas, nus, psis,
-           max_iters, epss)
-}
